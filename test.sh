15e3c54e5e0c81a7be389d9ee11660c057523747
masakiHamamoto <hama.makki@gmail.com>
2016-11-04 07:23:50 +0900

    test

--- test.sh
+++ test.sh
@@ -2,6 +2,6 @@
 
 python -V
 source venv/bin/activate
-../venv/bin/python -V
-../venv/bin/python3 -V
-../venv/bin/python mybook/manage.py test
+venv/bin/python -V
+venv/bin/python3 -V
+venv/bin/python mybook/manage.py test
