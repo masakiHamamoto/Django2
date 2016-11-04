72b9ec8e561f6cb234e5fbee8fac7fcfe135e0e6
masakiHamamoto <hama.makki@gmail.com>
2016-11-04 21:05:11 +0900

    test

--- deploy.sh
+++ deploy.sh
@@ -10,8 +10,11 @@ eval "$(ssh-agent -s)"
 echo "$DEPLOY_KEY" > deploy_key.pem
 echo "This key should have push access"
 chmod 600 deploy_key.pem
+expect -c "
+set timeout 5
 spawn ssh-add deploy_key.pem
-expect \"Enter passphrase for deploy_key.pem: \"
+expect \"Enter passphrase for\"
 send \"test\n\"
+"
 #git remote add deploy $REPO_URI
 #git push deploy master
