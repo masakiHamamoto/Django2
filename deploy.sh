013613679f975574b66e04ada12a3f9481edaa2f
masakiHamamoto <hama.makki@gmail.com>
2016-11-04 19:47:08 +0900

    test

---
+++ deploy.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+echo "Start the ssh agent"
+eval "$(ssh-agent -s)" 
+echo "$DEPLOY_KEY" > deploy_key.pem
+echo "This key should have push access"
+chmod 600 deploy_key.pem
+ssh-add deploy_key.pem
+#git remote add deploy $REPO_URI
+#git push deploy master
