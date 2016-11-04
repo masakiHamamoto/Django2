5dc626dfee88ea9ddf99741c150677b7601978f6
masakiHamamoto <hama.makki@gmail.com>
2016-11-04 21:01:09 +0900

    test

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 echo "Install expect"
+expect --help
 #sudo apt-get -y install expect
 which expect
 which send
@@ -9,8 +10,7 @@ eval "$(ssh-agent -s)"
 echo "$DEPLOY_KEY" > deploy_key.pem
 echo "This key should have push access"
 chmod 600 deploy_key.pem
-ssh-add deploy_key.pem
-expect --help
+spawn ssh-add deploy_key.pem
 expect \"Enter passphrase for deploy_key.pem: \"
 send \"test\n\"
 #git remote add deploy $REPO_URI
